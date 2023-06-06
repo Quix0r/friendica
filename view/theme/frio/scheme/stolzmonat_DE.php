205947d2b5829bad16fe07b7f9bc47131e047baa
Roland Häder <roland@mxchange.org>
2023-06-07 01:13:14 +0200

    Added #Stolzmonat sub-theme for Germany

---
+++ view/theme/frio/scheme/stolzmonat_DE.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Name: #StolzMonat - German flag
+ * Author: @roland@f.haeder.net
+ *
+ * This is satire!
+ */
+$nav_bg = "#708fa0";
+$nav_icon_color = "#fff";
+$link_color = "#6fdbe8";
+$background_color = "#ededed";
+$contentbg_transp = 100;
