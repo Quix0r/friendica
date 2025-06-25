8671b110e391cc167c4733f97852c9111031d7a2
Roland Häder <roland@mxchange.org>
2025-06-25 04:54:16 +0200

    Continued: - "black/white" are not working anymore

--- view/theme/frio/scheme/stolzmonat_DE.php
+++ view/theme/frio/scheme/stolzmonat_DE.php
@@ -5,10 +5,10 @@
  *
  * This is satire!
  */
-$nav_bg = "black";
-$nav_icon_color = "white";
+$nav_bg = "#000000";
+$nav_icon_color = "#ffffff";
 $background_color = "#ededed";
 $link_color = "#d4333d";
 $contentbg_transp = 100;
-$nav_icon_hover_color = "black";
-$btn_primary_hover_color = "black";
+$nav_icon_hover_color = "#000000";
+$btn_primary_hover_color = "#000000";
