df7c3783de2ed8d8a772e3e469fe6de6f5516329
Roland Häder <roland@mxchange.org>
2025-06-25 02:57:18 +0200

    Need to add `DBStructure` or else it cannot be found.

--- update.php
+++ update.php
@@ -38,6 +38,7 @@ use Friendica\Core\Update;
 use Friendica\Core\Worker;
 use Friendica\Database\Database;
 use Friendica\Database\DBA;
+use Friendica\Database\DBStructure;
 use Friendica\DI;
 use Friendica\Model\Attach;
 use Friendica\Model\Contact;
